99280404202b04943c76c0b368bd275299bfa5b0
John A. Pérez R <japr893@gmail.com>
2023-05-20 14:36:15 -0400

    TotalExpense showChart 0

--- src/components/ExpenseChart.jsx
+++ src/components/ExpenseChart.jsx
@@ -4,18 +4,49 @@ import { useGlobalState } from "../context/GlobalState";
 function ExpenseChart() {
   const { transactions } = useGlobalState();
 
-  const totalIncome = transactions.filter(transaction => transaction.amount > 0)
+  const totalIncome = transactions
+    .filter((transaction) => transaction.amount > 0)
     .reduce((acc, transaction) => (acc += transaction.amount), 0);
-    const totalExpenses = transactions.filter(transaction => transaction.amount < 0)
-    .reduce((acc, transaction) => (acc += transaction.amount), 0) * -1;
+  const totalExpenses =
+    transactions
+      .filter((transaction) => transaction.amount < 0)
+      .reduce((acc, transaction) => (acc += transaction.amount), 0) * -1;
 
-    const totalExpensesPorcentage = Math.round((totalExpenses / totalIncome) * 100);
+  const totalExpensesPorcentage = Math.round(
+    (totalExpenses / totalIncome) * 100
+  );
 
-    const totalIncomePercentage = 100 - totalExpensesPorcentage;
+  const totalIncomePercentage = 100 - totalExpensesPorcentage;
+
+  const showChart = totalIncome !== 0 || totalExpenses !== 0;
+
+  if (!showChart) {
+    return (
+      <>
+        <p className='text-center'>No hay datos disponibles para mostrar el gráfico.</p>
+        <VictoryPie
+          colorScale={["#52525b"]}
+          data={[{ x: "No Data", y: 100 }]}
+          animate={{
+            duration: 1000,
+          }}
+          labels={({datum}) => `${datum.y}%`}
+          labelComponent={
+            <VictoryLabel
+              angle={45}
+              style={{
+                fill: "#52525b",
+              }}
+            />
+          }
+        />
+      </>
+    )
+  }
 
   return (
     <VictoryPie
-      colorScale={["#e74c3c", "#2ecc71"]}
+      colorScale={totalExpenses < 0 ? ["#e74c3c", "#52525b"] : ["#e74c3c", "#2ecc71"]}
       data={[
         { x: "Expenses", y: totalExpensesPorcentage },
         { x: "Incomes", y: totalIncomePercentage },
