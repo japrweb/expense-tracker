205665e9f97c633bc8207b4178a08c58c1792ec1
John A. Pérez R <japr893@gmail.com>
2023-05-21 16:52:50 -0400

    Responsive styling

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ import ExpenseChart from './components/ExpenseChart';
 function App() {
   return (
     <GlobalProvider>
-      <div className='bg-zinc-950 text-white h-screen flex justify-center items-center'>
-        <div className='container mx-auto w-3/6'>
-          <div className='bg-zinc-800 p-10 rounded-lg flex gap-x-8'>
+      <div className='bg-zinc-950 text-white h-full flex justify-center items-center'>
+        <div className='container m-auto md:w-full max-w-screen-xl mx-8 my-8'>
+          <div className='bg-zinc-800 p-10 rounded-lg flex flex-col md:flex-row gap-x-8'>
             <div>
               <h1 className='text-4xl font-bold'>Expense Tracker</h1>
               <IncomeExpenses />
