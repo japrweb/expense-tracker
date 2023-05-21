9f9a891cb52cf1710a2befb53f82508ce3a13c9e
John A. Pérez R <japr893@gmail.com>
2023-05-21 17:30:58 -0400

    Margin changes completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { GlobalProvider } from './context/GlobalState';
 
-import Header from './components/Header';
 import Balance from './components/Balance';
 import TransactionForm from './components/transactions/TransactionForm';
 import TransactionList from './components/transactions/TransactionList';
@@ -13,14 +12,14 @@ function App() {
     <GlobalProvider>
       <div className='bg-zinc-950 text-white min-h-screen flex flex-col justify-center items-center px-4 py-4 md:px-8 md:py-8'>
         <div className='container m-auto md:w-full max-w-screen-xl'>
-          <div className='bg-zinc-800 p-10 rounded-lg flex flex-col md:flex-row gap-x-8'>
+          <div className='bg-zinc-800 p-10 rounded-lg flex flex-col md:flex-row gap-x-10'>
             <div>
               <h1 className='text-4xl font-bold'>Expense Tracker</h1>
               <IncomeExpenses />
               <Balance />
               <TransactionForm />
             </div>
-            <div className='flex flex-col flex-1 md:ml-4 m-auto'>
+            <div className='flex flex-col flex-1'>
               <ExpenseChart />
               <TransactionList />
             </div>
